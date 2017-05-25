01d6b3d7114cadf9acd7fb2101327436a02279d9
Lukasz Rogowicz <lucas@deertone.pro>
2017-05-25 14:49:27 +0200

    adding update form and delete button

--- api/src/Book.php
+++ api/src/Book.php
@@ -1,19 +1,15 @@
 <?php
-
 class Book implements JsonSerializable {
-
     private $id;
     private $title;
     private $author;
     private $description;
-
     public function __construct() {
         $this->id = -1;
         $this->setAuthor('');
         $this->setTitle('');
         $this->setDescription('');
     }
-
     public function create(PDO $conn) {
         $stmt = $conn->prepare('INSERT INTO books SET author=:author, title=:title, description=:description');
         $result = $stmt->execute([
@@ -29,41 +25,53 @@ class Book implements JsonSerializable {
             return [];
         }
     }
-
-    public function update(PDO $conn) {
-        
+    //funkcja aktualizująca tytuł wybranej książki
+    public function update(PDO $conn, $title ) {
+        $id = $this->getId();
+        $stmt = $conn->prepare('UPDATE books SET title=:title WHERE id=:id');
+        $result = $stmt->execute([
+            'id' => $id,
+            'title' => $title
+        ]);
+        if ($result === true) {
+            return [json_encode($this)];
+        } else {
+            return [];
+        }
     }
-
     public function delete(PDO $conn) {
-        
+        $id = $this->getId();
+        $sql = "DELETE FROM books WHERE id=:id";
+        $stmt = $conn->prepare($sql);
+        $result = $stmt->execute(['id' => $id]);
+        if ($result === true) {
+            $this->id = -1;
+            return [json_encode($this)];
+        } else {
+            return [];
+        }
     }
-
     //funkcja ladujaca pojedynczy wiersz
     static public function loadFromDb(PDO $conn, $id) {
         $stmt = $conn->prepare('SELECT * FROM books WHERE id=:id');
         $result = $stmt->execute(['id' => $id]);
-
         if ($result && $stmt->rowCount() > 0) {
             $row = $stmt->fetch();
-
             $book = new Book();
             $book->id = $row['id'];
             $book->author = $row['author'];
             $book->title = $row['title'];
             $book->description = $row['description'];
-
             //implementujemy interfejs ponieważ nie można zrobić jsona z obiektu
-            return [json_encode($book)];
+            return $book;
         } else {
             return [];
         }
     }
-
     //funkcja pobierajaca wszystkie wiersze
     static public function loadAllFromDb(PDO $conn) {
         $result = $conn->query('SELECT * FROM books');
         $books = [];
-
         //iterumey po rekordach z bazy
         foreach ($result as $row) {
             //tworzymy do kazdego rekordu obiekt ksiązki
@@ -72,14 +80,11 @@ class Book implements JsonSerializable {
             $book->author = $row['author'];
             $book->title = $row['title'];
             $book->description = $row['description'];
-
             //i wrzucamy go do tablicy
             $books[] = json_encode($book);
         }
-
         return $books;
     }
-
     public function jsonSerialize() {
         //metoda interfejsu
         //ta tablica bedzie zwrocona przy przekazaniu obiektu do json_encode()
@@ -90,33 +95,25 @@ class Book implements JsonSerializable {
             'description' => $this->description,
         ];
     }
-
     public function getId() {
         return $this->id;
     }
-
     public function getTitle() {
         return $this->title;
     }
-
     public function getAuthor() {
         return $this->author;
     }
-
     public function getDescription() {
         return $this->description;
     }
-
     public function setTitle($title) {
         $this->title = $title;
     }
-
     public function setAuthor($author) {
         $this->author = $author;
     }
-
     public function setDescription($description) {
         $this->description = $description;
     }
-
 }
--- js/app.js
+++ js/app.js
@@ -1,198 +1,153 @@
 $(function () {
-
+    //loading all books from database
     var divBooks = $('div#books');
-    //pobieramy ksiazki po zaladowaniu strony
+
     $.ajax({
         url: 'api/books.php',
         dataType: 'json'
     }).done(function (bookList) {
-        //bookList - tablica obiektow ksiazek w json
+
         bookList.forEach(function (singleBookJson) {
             var singleBook = JSON.parse(singleBookJson);
-            //tworzymy element li z nową książk
-            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><div class="bookDescription" style="margin-top:20px; font-weight: bold; color: blue; font-size: 13px;"></div></div><hr>');
-            //dodajemy element do listy
+            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><button class="btn remove btn-danger pull-right">Usuń</button><div class="bookDescription" style="margin-top:20px; font-weight: bold; font-size: 15px;"></div><hr></hdiv>');
             divBooks.append(newLi);
         });
     }).fail(function () {
         console.log('Error');
     });
 
-    //dodajemy event na tytul książki
-
-    //sposob na dodawanie eventow do elementow ladowanych dynamicznie
-    //event zakladamy na dowolnego przodka NIE ZALADOWANEGO DYNAMICZNIE
-    //jako drugi argument metody on() podajemy SELEKTOR (string) 
-    //elementu dynamicznie ladowanego na jakim event nas interesuje
-    //$(this) to element z drugiego argumentu
     divBooks.on('click', 'span.bookTitle', function () {
-        //pobieramy id książki z datasetu
+        //id from dataset
         var span = $(this);
         var bookId = span.parent().data('id');
-        //pobieramy opis książki
+        //description book
+        var spanDescription = span.siblings('.bookDescription');
+        if (spanDescription.children().length) {
+            spanDescription.toggle();
+        }
 
         $.ajax({
             url: 'api/books.php?id=' + bookId,
             dataType: 'json'
         }).done(function (bookList) {
-            //bookList - tablica obiektow ksiazek w json
-            //w tym wypadku tablica 1 elementowa
-            //poneiwaz pobieramy 1 ksiązke po id
-            var singleBook = JSON.parse(bookList[0]);
-            span.next().text(singleBook.description);
+            //bookList - table full of JSON OBJECT in this case just one object inside
+            var singleBook = bookList;
+            span.next().next().html('<span class="author">' + singleBook.author + '</span><br>' + '<span class="description">' + singleBook.description + '</span><hr><button class="btn edit btn-primary">Edytuj</button>');
         }).fail(function () {
             console.log('Error');
         });
     });
 
-    //DODAWANIE KSIĄŻKI
-
+    //ADD
     var addBook = $('#addBook');
     addBook.on('click', function (e) {
-        e.preventDefault();//KONIECZNE - poniewaz wysyłamy ajaxem
-        //pobieramy sobie dane z formularza
-        var form = $(this).parent();//zapisuje do zmiennej formularz
+
+        e.preventDefault();
+
+        var form = $(this).parent();//save from to variable
 
         var author = form.find('input[name=author]').val();
         var title = form.find('input[name=title]').val();
         var description = form.find('textarea[name=description]').val();
 
-        //tworzymy obiekt z danymi do wysłania
+        //new object to send
         var sendObj = {};
         sendObj.author = author;
         sendObj.title = title;
         sendObj.description = description;
-        //wysylamy żądanie ajax o dodanie książki
         $.ajax({
-            url: 'api/books.php', //adres na jaki wysyłamy
-            dataType: 'json', //typ danych ZWRACANYCH
-            data: sendObj, //obiekt z danymi
-            type: 'POST'//dodajemy więc POST (REST)
+            url: 'api/books.php', //adres to send
+            dataType: 'json', //type of returning data
+            data: sendObj, //object with data
+            type: 'POST'// set POST because of (REST)
         }).done(function (bookList) {
             var singleBook = JSON.parse(bookList[0]);
-            //tworzymy element li z nową książk
-            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><div class="bookDescription" style="margin-top:20px; font-weight: bold; color: blue; font-size: 13px;"></div></div><hr>');
-            //dodajemy element do listy
+
+
+            var newLi = $('<div data-id="' + singleBook.id + '"><span class="bookTitle">' + singleBook.title + '</span><button type="button" class="remove btn btn-danger pull-right">Usuń</button><div class="bookDescription" style="margin-top:20px; font-weight: bold; font-size: 13px;"></div></div><hr>');
             divBooks.append(newLi);
         }).fail(function () {
 
         });
     });
-    
-    //AKTUALIZACJA KSIĄŻEK (PUT)
-    //1) do każdego diva z opisem (lub dodatkowego pod nim) dodajemy formularz edycji książki
-
-    // var newForm = $('<form data-id="' + singleBook.id + '"><input type="text" value="' + singleBook.title + '"></form>');
-    // divBooks.append(newForm);
-    //
-    // //2) formualrz jest ukryty domyslnie i zaladowany danymi ksiazki val('wartosc')
-    // //3) dodajemy przycisk do edycji analogicznie jak w DELETE
-    // //4) zakladamy event na przycisk (pamietamy ze jest ladowany dynamicznie)
-    // //5) po kliknieciu pokazujemy formualarz
-    // //6) po klikeniciu w submit formularza pobieramy z niego dane (podobnie jak POST)
-    // //7) tworzymy obiekt do wyslania (jak w POST) - dodać ID!!!!
-    // //8) wysylamy ajaxem metodą PUT przekazujac obiekt (PAMIĘTAMY o dodaniu ID
-    // //9) pobieramy z bazy obiekt książki na backendzie z aktualnymi danymi
-    // //10) aktualizujemy seterami dane w obiekcie
-    // //11) wywołujemy metodę update()
-    // //12) jak się uda to w drzewie dom aktualizujemy tytul i opis elementu
-    //
-    // //USUWANIE KSIĄŻEK (DELETE)
-    // var deleteButton = $('#deleteButton');
-    // deleteButton.one('click', 'dynamicznie', function () {
-
-    });
-    //1) dodajemy przycisk obok tytułu z usuwaniem podczas tworzenia ksiazki
-    //pamietamy zeby dodac przycisk jak ladujemy przy ladowaniu strony i dodaniu (POST)
-    //2) zakladamy event na przycisk (pamietamy ze on jest ladowany dynamicznie)
-    //po kliknieciu pobieramy id ksiazki (rodzic) i wysylamy do ajaxa za pomocą
-    //metody DELETE (bardzo podobnie jak GET)
-    //3) pobieracie ksiązkę z bazy odpowiednią metoda po id (filtrujemy na int)
-    //4) mając obiekt ksiązki wywołujemy metodę delete()
-    //5) jeśli się uda to usuwamy książkę z drzewa DOM
-
-    //--------------------------------------------------------------------------
-    // Deleting book
-    divBooks.on('click', 'button.delete', function (e) {
-        e.preventDefault();
-        var button = $(this); //delete button object
-        var bookId = button.parent().data('id'); //id of book, from it div
-        // AJAX request
-        $.ajax({
-            url: 'api/books.php',
-            dataType: 'json',
-            data: 'id=' + bookId,
-            type: 'DELETE'
-        }).done(function (deletedBookArray) { //deletedBookarray is array with object of deleted book with id -1 or nothing
-            if (deletedBookArray) {
-                button.parent().remove();
-            }
-        }).fail(function () {
-            console.log('Error');
-        });
-    });
-
-    //--------------------------------------------------------------------------
-    //Editing book
+    //EDIT
+    //to edit book i need to set event on edit button, but this button doesn't exist so i need to set event on him existing parent
     divBooks.on('click', 'button.edit', function (e) {
         e.preventDefault();
-        //get id from book div we are in
+        // looking for some specific data of existing element
         var id = $(this).parent().parent().data('id');
-        //get elements with book information
         var title = $(this).parent().siblings('span.bookTitle');
         var author = $(this).siblings('span.author');
         var description = $(this).siblings('span.description');
 
-        //--------------------------------
-        //Create form, set id of book
-        var editForm = $('<div class="edit" data-id="' + id + '"><form>Autor:<br><input type="text" name="author" value=""/><br>Tytuł:<br><input type="text" name="title" value=""/><br>Opis:<br><textarea name="description" value=""></textarea><br><button class="editBook">Edytuj książkę</button></form></div>');
-        //get inputs from form
-        var formTitle = editForm.find('input[name=title]');
-        var formAuthor = editForm.find('input[name=author]');
-        var formDescription = editForm.find('textarea[name=description]');
-        //set values in form
+        //adding form to edit choosen book
+        var formToEdit = $('<hr><div class="editForm form-group" data-id"' + id + '"><form>Autor:<br><input type="text" class="form-control" name="author"/><hr>Tytuł:<br><input type="text" name="title" class="form-control"/><hr>Opis:<br><textarea name="description" class="form-control"></textarea><hr><button type="button" class="btn editBook btn-primary">Zatwierdź zmiany</button></form></div>');
+        // after showed edit form, looking for input
+        var formTitle = formToEdit.find('input[name=title]');
+        var formAuthor = formToEdit.find('input[name=author]');
+        var formDescription = formToEdit.find('textarea[name=description]');
+        // fill this input by currently existing data from major div with description
         formTitle.val(title.text());
         formAuthor.val(author.text());
         formDescription.val(description.text());
-        //---------------------------------
-
-        //Add event on button from edit form
-        editForm.on('click', 'button.editBook', function (e) {
+        //form can appear and disappear
+        if ($(this).siblings('div.editForm').length) {
+            $(this).siblings('div.editForm').toggle();
+        } else {
+            formToEdit.insertAfter($(this));
+        }
+        //this event is on the submit button
+        formToEdit.on('click', 'button.editBook', function (e) {
             e.preventDefault();
-
-            //get current values, and insert them to object
+            //creating object with data provided from input form and sending by Ajax to database
             var editObj = {};
             editObj.id = id;
             editObj.title = formTitle.val();
             editObj.author = formAuthor.val();
             editObj.description = formDescription.val();
-
-            //AJAX request
+            //use Ajax from with JSON format of data
             $.ajax({
                 url: 'api/books.php',
                 dataType: 'json',
                 data: editObj,
                 type: 'PUT'
-            }).done(function (editBookArray) { //editBookArray is an array with object of edited book, or nothing
-                editBook = editBookArray[0]; //server returns array
-                //update values on page
+            }).done(function (editBook) {
+                editBook = JSON.parse(editBook[0]);
+                //if success put new value in the existing span with all description
                 title.text(editBook.title);
                 author.text(editBook.author);
                 description.text(editBook.description);
+
             }).fail(function () {
                 console.log('Error');
             });
 
         });
-
-        // Append form
-        if ($(this).siblings('div.edit').length) {
-            $(this).siblings('div.edit').toggle();
-        } else {
-            editForm.insertAfter($(this));
-        }
-
+    });
+    //DELETE
+    //event is on the div id books because button doesn't exist onload page
+    divBooks.on('click', 'button.remove', function (e) {
+        //prevent default because this is button
+        e.preventDefault();
+        var button = $(this);
+        // search value of data-id
+        var bookId = button.parent().data('id');
+        //create variable for sending to php
+        var info = 'id=' + bookId;
+        //using Ajax to delete
+        $.ajax({
+            url: 'api/books.php',
+            dataType: 'json',
+            data: info,
+            type: 'DELETE'
+        }).done(function (success) {
+            if (success) {
+                //remove div with all books description
+                button.parent().remove();
+            }
+        }).fail(function () {
+            console.log('Something went wrong');
+        });
     });
 
-});
 });
